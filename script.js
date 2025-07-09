04332408ec722d13488521a98821e68778e05d02
theresah95 <hoelketheresa@gmail.com>
2025-07-09 13:20:41 -0700

    changed the id to be generated in the constructor instead of addBookToLibrary

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const myLibrary = [];
 
 //Constructor for books
-function Book(title, author, numPages, status, id) {
+function Book(title, author, numPages, status) {
     if(!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
@@ -10,7 +10,7 @@ function Book(title, author, numPages, status, id) {
     this.author = author;
     this.numPages = numPages;
     this.status = status
-    this.id = id;
+    this.id = crypto.randomUUID;
 
     this.printBookInfo = function() { 
         console.log(`${this.title}\n${this.author}\n${this.numPages}\n${this.status}\n${this.id}`);
@@ -20,7 +20,7 @@ function Book(title, author, numPages, status, id) {
 function addBookToLibrary(title, author, numPages, status){
     //generate a unique id for the book
     let id = crypto.randomUUID();
-    let book = new Book(title, author, numPages, status, id);
+    let book = new Book(title, author, numPages, status);
     myLibrary.push(book);
 }
 
