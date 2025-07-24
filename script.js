0a9a144ba79c62342d26dccf5c430fddc91fd4ea
theresah95 <hoelketheresa@gmail.com>
2025-07-24 10:19:14 -0700

    Added functionality to submitAction to allow the form to be reset after every submit; also added comments to the function for clarity of each step

--- script.js
+++ script.js
@@ -48,21 +48,26 @@ const submit = document.querySelector("#submit-button");
 submit.addEventListener("click", submitAction);
 
 function submitAction(event){
+    //Prevents submitting form
     event.preventDefault();
+    //Collects relevant info then adds book to myLibrary[]
     let title, author, numPages, status; 
     title = document.getElementById("title").value;
     author = document.getElementById("author").value;
     numPages = document.getElementById("num-pages").value;
     status = document.getElementById("status").value;
-
-    const newBook = addBookToLibrary(title, author, numPages, status);
-    myLibrary.at(-1).printBookInfo();   
+    addBookToLibrary(title, author, numPages, status);
+    //To test and ensure the correct values are being added to the array 
+    // myLibrary.at(-1).printBookInfo();   
     // printLibrary();
-
+    //Adds new book from back of the myLibrary array to the grid 
     const newGridItem = document.createElement('div');
     newGridItem.classList.add('book');
     newGridItem.textContent = myLibrary.at(-1).title;
     gridContainer.appendChild(newGridItem);
+    //Resets input to empty so that you can add another book
+    document.getElementById('form').reset();
 };
+
 
 
